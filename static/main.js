4ce9581fd7980036c4bec6d6fda71e97613615d1
shrapnelnet <shrapnelnet@protonmail.com>
2023-05-13 18:21:45 +0100

    add listening to section

---
+++ static/main.js
@@ -0,0 +1,42 @@
+const getSong = () => {
+    document.body.style.display = "none";
+    fetch("https://cors-anywhere.herokuapp.com/https://europe-west2-tyler-dev-env.cloudfunctions.net/lastfm-get-song", {
+        headers: {
+            "Access-Control-Allow-Origin": "*"
+        }
+    })
+        .then((res) => res.json())
+        .then((res) => {
+            const root = res.recenttracks.track[0];
+            const image = root.image[2]["#text"];
+            const name = root.name;
+            const artist = root.artist["#text"];
+            return {
+                image: image,
+                name: name,
+                artist: artist
+            }
+        })
+        .then((song) => {
+            let image = document.createElement("img");
+            image.src = song.image;
+            image.width = 174;
+            image.height = 174;
+            let name = document.createElement("h3");
+            name.innerText = song.name;
+            let artist = document.createElement("p");
+            artist.innerText = song.artist;
+            const metadata = [image, name, artist];
+            let listeningTo = document.getElementById("lastfm");
+            metadata.forEach((data) => {
+                listeningTo.appendChild(data);
+            })
+            document.body.style.display = "block";
+        })
+        .catch((err) => {
+            console.error(err);
+            document.body.style.display = "block";
+        });
+}
+
+getSong();
